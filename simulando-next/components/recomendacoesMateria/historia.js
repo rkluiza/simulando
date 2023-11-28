8d4fa50246124bb6f22f3ec1687389784e3fcdd6
Julia Dapper <juliadapper.sg060@academico.ifsul.edu.br>
2023-11-27 23:31:29 -0300

    Update historia.js

--- simulando-next/components/recomendacoesMateria/historia.js
+++ simulando-next/components/recomendacoesMateria/historia.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default function Historia() {
     return (
@@ -5,49 +6,55 @@ export default function Historia() {
             <li class="list-group-item d-flex justify-content-between align-items-start">
                 <div class="ms-2 me-auto">
                     <div class="fw-bold">Pré-História (Brasil e mundo)</div>
-                    Content for list item
+                    <Link href="https://www.todamateria.com.br/pre-historia-brasileira/">Pré-história brasileira</Link><br />
+                    <Link href="https://www.todamateria.com.br/pre-historia-mundial/">Pré-história mundial</Link>
                 </div>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-start">
                 <div class="ms-2 me-auto">
                     <div class="fw-bold">Idade Antiga - as civilizações antigas ocidentais e orientais até o momento da crise do império romano</div>
-                    Content for list item
+                    <Link href="https://www.todamateria.com.br/idade-antiga/">Idade Antiga</Link><br />
                 </div>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-start">
                 <div class="ms-2 me-auto">
                     <div class="fw-bold">Idade Média - do surgimento do feudalismo até o nascimento da modernidade</div>
-                    Content for list item
+                    <Link href="https://www.todamateria.com.br/idade-media/">Idade Média</Link><br />
+                    <Link href="https://querobolsa.com.br/enem/historia-geral/idade-media">Idade Média</Link>
                 </div>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-start">
                 <div class="ms-2 me-auto">
                     <div class="fw-bold">Idade Moderna - da modernidade à crise do antigo regime</div>
-                    Content for list item
+                    <Link href="https://mundoeducacao.uol.com.br/historiageral/idade-moderna.htm">Idade Moderna</Link><br />
+                    <Link href="https://www.todamateria.com.br/idade-moderna/">Idade Moderna</Link>
                 </div>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-start">
                 <div class="ms-2 me-auto">
                     <div class="fw-bold">Idade Contemporânea - as relações sociais, econômicas, políticas, culturais típicas do século XIX, XX e XXI</div>
-                    Content for list item
+                    <Link href="https://www.todamateria.com.br/idade-contemporanea/">Idade Contemporânea</Link><br />
                 </div>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-start">
                 <div class="ms-2 me-auto">
                     <div class="fw-bold">Brasil e América colonial</div>
-                    Content for list item
+                    <Link href="https://descomplica.com.br/d/vs/aula/america-colonialx/">Brasil Colônia</Link><br />
+                    <Link href="https://www.politize.com.br/brasil-colonia/">Brasil Colônia</Link><br />
                 </div>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-start">
                 <div class="ms-2 me-auto">
                     <div class="fw-bold">Américas no século XIX – Da independência do Brasil à implantação da República</div>
-                    Content for list item
+                    <Link href="https://brasilescola.uol.com.br/historia-da-america/os-estados-unidos-no-seculo-xix.htm">Américas no século XIX</Link><br />
+                    <Link href="https://descomplica.com.br/d/vs/historia/mundo-contemporaneo/as-independencias-na-america-e-suas-consequencias-ybg6kf/">Américas no século XIX</Link><br />
                 </div>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-start">
                 <div class="ms-2 me-auto">
                     <div class="fw-bold">Brasil republicano</div>
-                    Content for list item
+                    <Link href="https://www.todamateria.com.br/brasil-republica/">Brasil República</Link><br />
+                    <Link href="https://www.todapolitica.com/brasil-republica/">Brasil República</Link><br />
                 </div>
             </li>
         </ol>
